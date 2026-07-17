8f8552c81464beb79ec60070faccdf11b136a632
root <root@ip-172-31-14-100.ec2.internal>
2026-07-17 08:02:22 +0000

    updated the app.java file with code

---
+++ app.java
@@ -0,0 +1,9 @@
+// The class name must exactly match the file name (HelloWorld.java)
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints the message to the console terminal
+        System.out.println("Hello, World!");
+    }
+}
+
+##############################################################
